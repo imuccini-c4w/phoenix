bdea6d1d225e26ce5bc5cdb278823032904609ec
imuccini-c4w <imuccini@cloud4wi.com>
2025-12-03 20:14:47 +0100

    favico fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export const metadata: Metadata = {
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
   },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
